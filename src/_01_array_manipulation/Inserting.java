20123b294cad96944f5fdd59abb86b2ef77f1931
dhruvwa16 <League@AdministorsiMac.attlocal.net>
2018-04-01 17:32:08 -0700

    Start old fashioned arrays level4

--- src/_01_array_manipulation/Inserting.java
+++ src/_01_array_manipulation/Inserting.java
@@ -1,7 +1,15 @@
 package _01_array_manipulation;
 
-
-
 public class Inserting {
-	
+
+	public static int[] insertAt(int[] testArray, int location, int num) {
+	int[] temp = new int[testArray.length+1];
+	for (int i = 0; i < temp.length; i++) {
+		temp[i] = ;
+	}
+			testArray[location] = num;
+		
+		return testArray;
+	}
+
 }
--- src/_01_array_manipulation/Sorting.java
+++ src/_01_array_manipulation/Sorting.java
@@ -2,4 +2,20 @@ package _01_array_manipulation;
 
 public class Sorting {
 
+	public static String[] sort(String[] testArray) {
+		for (int i = 0; i < testArray.length; i++) {
+			for (int j = i + 1; j < testArray.length; j++) {
+				if (testArray[i].compareTo(testArray[j]) > 0) {
+					String temp = testArray[i];
+					testArray[i] = testArray[j];
+					testArray[j] = temp;
+					// System.out.println(temp);
+					// System.out.println(testArray[j]);
+				}
+			}
+
+		}
+		return testArray;
+	}
+
 }
