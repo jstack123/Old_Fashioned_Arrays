7fe2bb58e6de1b158e5d9cd1067bcb972fb68877
jstack123 <league@WTS-IM-17.attlocal.net>
2018-04-15 17:32:09 -0700

    finished array manipulation

--- src/_01_array_manipulation/Inserting.java
+++ src/_01_array_manipulation/Inserting.java
@@ -3,27 +3,53 @@ package _01_array_manipulation;
 public class Inserting {
 
 	public static int[] insertAt(int[] testArray, int location, int num) {
-	int[] temp = new int[testArray.length+1];
-	
-	for (int i = 0; i < temp.length; i++) {
-		if (i<temp.length) {
-			
-		if (i==location) {
-			temp[location] = num;
-		} else if (i<location) {
-			temp[i] = testArray[i];
-		}else if(i>location) {
-			temp[i] = testArray[i-1];
+		int[] temp = new int[testArray.length + 1];
+
+		for (int i = 0; i < temp.length; i++) {
+			if (i < temp.length) {
+
+				if (i == location) {
+					temp[location] = num;
+				} else if (i < location) {
+					temp[i] = testArray[i];
+				} else if (i > location) {
+					temp[i] = testArray[i - 1];
+				}
+			}
+			System.out.println(temp[i]);
 		}
-	}
-		System.out.println(temp[i]);
-	}
-	
-		
-	
-			 //testArray = testArray.length-1;
-			
+
+		// testArray = testArray.length-1;
+
 		return temp;
+	}
+
+	public static String[] insertAlphabetically(String[] orderedArray, String word) {
+		String[] newArray = new String[orderedArray.length + 1];
+
+		for (int i = 0; i < orderedArray.length; i++) {
+			/*
+			 * for (int j = i+1; j < orderedArray.length; j++) { if
+			 * (orderedArray[i].compareTo(orderedArray[j])>0) { String temp =
+			 * orderedArray[i]; orderedArray[i] = orderedArray[j]; orderedArray[j] = temp; }
+			 * 
+			 * }
+			 */
+
+			/*
+			 * if (i==0) { newArray[0] = word; } else if(i==newArray.length-1) {
+			 * newArray[newArray.length-1] = word; } else { newArray[i] = orderedArray[i-1];
+			 * 
+			 * }
+			 */
+
+			newArray[i] = orderedArray[i];
+		
+			System.out.println(orderedArray[i]);
+		}
+		newArray[newArray.length-1] = word;
+		Sorting.sort(newArray);
+		return newArray;
 	}
 
 }
